d5ce9154db857bed944490d31246e4de0661fc68
Muhannad <Muhannadalmughrabi233@gmail.com>
2021-03-29 18:38:51 +0300

    finished feature2

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 'use strict';
+// Load Environment Variables from the .env file
+require('dotenv').config();
+
 // Load the express module into our script
 const express = require('express');
-const cors = require('cors');
 const superagent = require('superagent')
+const cors = require('cors');
+const locations = {};
 
-require('dotenv').config();
 
 // App Setup:
 const app = express(); //creating the server application
 const PORT = process.env.PORT || 3000;
-
 app.use(cors()); //will respond to any request and allow access to our api from another domain
 
 /*
@@ -36,25 +38,34 @@ app.use('*', notFoundHandler);
 // Location callback
 function handleLocation(req, res) {
 
-        const city = req.query.city;
-        let key = process.env.GEOCODE_API_KEY;
-        const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+    let city = req.query.city;
+    let key = process.env.GEOCODE_API_KEY;
+    const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+
+    if (locations[url]) {
+        // send the data we currently have
+        res.send(locations[url]);
+    } else {
         superagent.get(url)
-        .then(data => {
-            const geoData = data.body[0];
-            const locationInfo = new Location(city, geoData);
-            res.status(200).send(locationInfo);
-        })
-        .catch((error) => errorHandler(error, request, response))
-       
+            .then(data => {
+                console.log(data);
+                const geoData = data.body[0];
+                const locationInfo = new Location(city, geoData);
+                locations[url] = locationInfo;
+                res.send(locationInfo);
+            })
+            .catch((err) => errorHandler(err, request, response));
+    }
+
+
 }
 
 //location constructor:
 function Location(city, geoData) {
     this.search_query = city;
-    this.formatted_query = geoData[0].display_name;
-    this.latitude = geoData[0].lat;
-    this.longitude = geoData[0].lon;
+    this.formatted_query = geoData.display_name;
+    this.latitude = geoData.lat;
+    this.longitude = geoData.lon;
 }
 
 
@@ -65,7 +76,7 @@ function handleWeather(req, res) {
         const weatherRender = [];
         weather.data.map(day => {
             weatherRender.push(new Weather(day));
-        
+
         })
         res.status(200).json(weatherRender);
 
